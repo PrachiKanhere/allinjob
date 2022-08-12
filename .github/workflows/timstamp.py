cf7e714626147a20b63eae3c0e8ef11d76628900
PrachiKanhere <92923938+PrachiKanhere@users.noreply.github.com>
2022-08-12 11:02:17 +0530

    Update timstamp.py

--- .github/workflows/timstamp.py
+++ .github/workflows/timstamp.py
@@ -16,10 +16,10 @@ est_time= now.astimezone(timezone('US/Eastern')).strftime("%H:%M:%S")
 zone = datetime.datetime.now(eastern).strftime("%Z")
 Current_year=datetime.date.today().strftime("%Y")
 TIMESTAMP= Day_of_week[0:3]+' '+Month_of_year[0:3]+' '+Day_of_the_month+' '+est_time+' '+zone+' '+Current_year
-#print(TIMESTAMP)
+print(TIMESTAMP)
 
 formattedDate = datetime.date.today().strftime("%d-%b-%Y")
-#print(formattedDate)
+print(formattedDate)
 
 match = re.search(r"Following Buses might be down: \[.*\]", str)
 if match:
